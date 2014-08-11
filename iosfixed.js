b7641ebf4fff57e5f100c39a232256b0b7a7111a
Andy Song <keechi@qq.com>
2014-08-11 17:58:00 +0800

    Update iosfixed.js

--- iosfixed.js
+++ iosfixed.js
@@ -1,10 +1,14 @@
 ;(function(win, doc, undefined) {
 
-  function IOSFixed() {
-  
+  function IOSFixed(dom) {
+    
   }
   
   IOSFixed.prototype = {
   
+  };
+  
+  window.iosfixed = function(dom) {
+    return new IOSFixed(dom);
   };
 })(window, document);
